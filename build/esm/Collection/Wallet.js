b63c23adfb2c815f0d14b038d4757d2388644dba
Matt Kenefick <keneficksays@gmail.com>
2023-10-01 09:23:26 -0400

    Update package

---
+++ build/esm/Collection/Wallet.js
@@ -0,0 +1,7 @@
+import { CollectionBase } from '@chalkysticks/sdk-core';
+import ModelWallet from '../Model/Wallet';
+export default class CollectionWallet extends CollectionBase {
+    endpoint = 'wallet';
+    model = ModelWallet;
+}
+//# sourceMappingURL=Wallet.js.map
---
+++ build/esm/Model/Wallet.js
@@ -0,0 +1,10 @@
+import { ModelBase } from '@chalkysticks/sdk-core';
+export default class ModelWallet extends ModelBase {
+    endpoint = 'wallet';
+    fields = [
+        'id',
+        'created_at',
+        'updated_at',
+    ];
+}
+//# sourceMappingURL=Wallet.js.map
